b9b5f000018b87c98068439e4c7ec2ff614f686e
Romain Fromi <romain.fromi@gmail.com>
2026-04-30 22:12:47 +0200

    Outline on meeple on forest cards

--- app/src/material/MeepleDescription.ts
+++ app/src/material/MeepleDescription.ts
@@ -1,10 +1,21 @@
-import { TokenDescription } from '@gamepark/react-game'
+import { css, Interpolation, Theme } from '@emotion/react'
+import { ItemContext, TokenDescription } from '@gamepark/react-game'
+import { MaterialItem } from '@gamepark/rules-api'
+import { LocationType } from '@gamepark/amanite/material/LocationType'
 import { PlayerAnimal } from '@gamepark/amanite/PlayerAnimal'
 import { MeepleHelp } from './help/MeepleHelp'
 import FoxMeeple from '../images/tokens/meeple/FoxMeeple.png'
 import SquirrelMeeple from '../images/tokens/meeple/SquirrelMeeple.png'
 import OwlMeeple from '../images/tokens/meeple/OwlMeeple.png'
 import JayMeeple from '../images/tokens/meeple/JayMeeple.png'
+
+const onTileOutlineCss = css`
+  filter:
+    drop-shadow(0.06em 0 0 black)
+    drop-shadow(-0.06em 0 0 black)
+    drop-shadow(0 0.06em 0 black)
+    drop-shadow(0 -0.06em 0 black);
+`
 
 class MeepleDescription extends TokenDescription {
   width = 1.6
@@ -18,6 +29,11 @@ class MeepleDescription extends TokenDescription {
     [PlayerAnimal.Squirrel]: SquirrelMeeple,
     [PlayerAnimal.Owl]: OwlMeeple,
     [PlayerAnimal.Jay]: JayMeeple
+  }
+
+  getItemExtraCss(item: MaterialItem, _context: ItemContext): Interpolation<Theme> {
+    if (item.location.type === LocationType.ForestTileMeepleSpot) return onTileOutlineCss
+    return
   }
 }
 
